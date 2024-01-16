0ce7a2d52744bf4c8ae320b9b4983b8d33464a7c
xiongyechang <xiongyechang@zuoyebang.com>
2024-01-16 16:58:45 +0800

    feat: 添加babel编译成es5

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  presets: [
+  "presets": [
     [
       "@babel/preset-env",
       {
-        // targets: "> 1%, last 2 versions, not ie <= 8",
-        modules: false,
-        useBuiltIns: "usage",
-        corejs: 3,
-      },
-    ],
-    // "@babel/preset-typescript",
-  ],
-  plugins: [
-    // "@babel/plugin-external-helpers",
-    [
-      "@babel/plugin-transform-runtime", {
-        "corejs": 3,
-        "useESModules": true,
+        "targets": "> 1%, last 2 versions, not ie <= 8",
+        "modules": false,
+        "useBuiltIns": "usage",
+        "corejs": 3
       }
     ]
   ],
+  "plugins": [
+    "@babel/plugin-transform-modules-commonjs",
+    "@babel/plugin-external-helpers",
+    [
+      "@babel/plugin-transform-runtime", {
+        "corejs": 3,
+        "useESModules": false
+      }
+    ]
+  ]
 }
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,88 +1,44 @@
-import { babel, getBabelOutputPlugin } from '@rollup/plugin-babel';
+import { getBabelOutputPlugin } from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
+import path from 'node:path';
+
+const dist = 'dist';
+
+const resolvePath = (filename) => path.join(process.cwd(), filename);
 
 export default {
   input: 'lib/index.ts',
   output: [
     {
-      dir: 'dist',
+      dir: dist,
       format: 'esm',
       entryFileNames: '[name].esm.min.js',
       plugins: [terser()]
-    }, 
+    },
     {
-      dir: 'dist',
-      format: 'iife',
-      entryFileNames: '[name].iife.min.js',
-      name: 'DataSynchronizer',
+      dir: dist,
+      format: 'esm',
+      entryFileNames: '[name].esm.js',
+    },
+    {
+      dir: dist,
+      format: 'cjs',
+      entryFileNames: '[name].cjs.min.js',
       plugins: [terser()]
     },
     {
-      dir: 'dist',
-      format: 'amd',
-      entryFileNames: '[name].amd.min.js',
-      plugins: [terser()]
-    },
-    {
-      dir: 'dist',
-      format: 'umd',
-      name: 'DataSynchronizer',
-      entryFileNames: '[name].umd.min.js',
-      plugins: [terser()]
-    },
-    {
-      dir: 'dist',
-      format: 'umd',
-      entryFileNames: '[name].min.js',
-      name: 'DataSynchronizer',
-      plugins: [terser()]
-    },
-    {
-      dir: 'dist',
-      format: 'system',
-      entryFileNames: '[name].system.min.js',
-      plugins: [terser()]
+      dir: dist,
+      format: 'cjs',
+      entryFileNames: '[name].cjs.js',
     },
   ],
   plugins: [
-    typescript({
-      tsconfig: 'tsconfig.json'
+    getBabelOutputPlugin({
+      configFile: resolvePath("babel.config.js"),
     }),
-    // babel({
-    //   configFile: "babel.config.js",
-    //   exclude: "node_modules/**",
-    //   babelHelpers: "runtime"
-    // })
-    // getBabelOutputPlugin(
-    //   {
-    //     presets: [
-    //       [
-    //         "@babel/preset-env",
-    //         {
-    //           targets: "> 1%, last 2 versions, not ie <= 8",
-    //           modules: false,
-    //           useBuiltIns: "usage",
-    //           corejs: 3,
-    //         },
-    //       ],
-    //       // [
-    //       "@babel/preset-typescript",
-    //         // {
-    //         //   allowDeclareFields: true,
-    //         // }
-    //       // ]
-    //     ],
-    //     plugins: [
-    //       "@babel/plugin-external-helpers",
-    //       [
-    //         "@babel/transform-runtime",
-    //         {
-    //           "useESModules": true
-    //         }
-    //       ]
-    //     ],
-    //   }      
-    // )
+    typescript({
+      tsconfig: resolvePath('tsconfig.json'),
+    }),
   ],
 };
