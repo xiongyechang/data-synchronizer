848b2fe529499e8e93ab5bc5d6e879f8433d01ae
xiongyechang <2226791767@qq.com>
2024-11-16 00:09:13 +0800

    fix: 修复regexp传输出错的问题

--- lib/format/regexp.ts
+++ lib/format/regexp.ts
@@ -3,10 +3,17 @@ const $type = 'regexp';
 const to = (value: RegExp) => {
   return [
     $type,
-    value.source,
+    value.toString(),
   ]
 }
 
-const from = (value: string) => new RegExp(value);
+const from = (value: string) => {
+  // 去掉字符串中的前后斜杠
+  const pattern = value.slice(1, value.lastIndexOf('/'));  // 获取模式部分
+  const flags = value.slice(value.lastIndexOf('/') + 1);  // 获取标志部分
+  // 使用 RegExp 构造函数创建正则表达式
+  const regex = new RegExp(pattern, flags);
+  return regex;
+}
 
 export default [$type, from, to];
